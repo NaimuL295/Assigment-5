fdd04ecf1a6be375a3aca5db7372beb5eca016b8
unknown <nayemk295@gmail.com>
2025-03-02 22:01:41 +0600

    styele fix

--- script/script.js
+++ script/script.js
@@ -1,62 +1,63 @@
 
 document.getElementById("next-page").addEventListener("click",function(){
   window.location.href="Blog.html"
-})
+});
 
 let year= new Date();
 let allTime=year.toDateString() ;
-let dateHMS=year.toLocaleTimeString()
-console.log(dateHMS);
+let dateHMS=year.toLocaleTimeString();
 
-//  let timeBox=getimervalue("time-box")
- let timeBox=document.getElementById("Deadline-box");
-timeBox.innerText=allTime
 
-const timeBoxtext=document.querySelectorAll("#time-box-text")
-for (const  element of timeBoxtext) {
-    element.innerText=allTime;
-}
+ let timeline=document.getElementById("Deadline-box");
+  timeline.innerText=allTime;
+
+
+ 
+// const timeBoxtext=document.querySelectorAll("#time-box-text")
+// for (const  element of timeBoxtext) {
+//     element.innerText=allTime;
+// }
 
 // color-generate
 
- let colorchange= document.getElementById("color-generate")
+
+// end
+ let colorchange= document.getElementById("color-generate");
 
  colorchange.addEventListener("click",function () {
+  
   let  color=Math.ceil( Math.random()*256 )
  let colos=`rgb(${color},${color},${color}) `;
 document.body.style.backgroundColor=colos
  })
 
-  const history = document.getElementById("add-history")
-
+  const history = document.getElementById("add-history");
   const btnBox = document.querySelectorAll("#btn-box"); 
   
 for (let  i= 0; i < btnBox.length; i++) {
-  
   btnBox[i].addEventListener("click",function(){
-
-  alert("Board updated Succussfully");
-
+    alert("Board updated Succussfully");
   // nav
 
-  let box1=[]
+  let box1=[] ;
 const textTitle=document.querySelectorAll("#box-title");
-box1=textTitle[i].textContent 
+box1=textTitle[i].textContent ;
   
   let navValue=getconvertvalue("nav-rigth");
   const navnum=navValue++
    document.getElementById("nav-rigth").innerText=navValue;
   //
-  
+
   // task
   let leftnum=getconvertvalue("left-text");
   
-  let  leftValue=leftnum-=1
+  let  leftValue=leftnum-=1;
   
 
-  if (leftValue<0) {
-    return alert("Board updated Succussfully");
+  if (leftValue<0 ) {
+      return alert("Board updated Succussfully");    
   }
+
   document.getElementById("left-text").innerText=leftValue;
   // left tex end
   
@@ -68,21 +69,24 @@ box1=textTitle[i].textContent
     at ${dateHMS}  </h5>`;
    
     history.appendChild(div)
-  btnBox[i].setAttribute("disabile",true)
-  btnBox[i].classList.remove("bg-blue-600")
-  btnBox[i].classList.add("bg-white")
-  btnBox[i].classList.add("text-gray-300")
-  
-  
+  btnBox[i].disabled =true
+  btnBox[i].classList.remove("bg-blue-600");
+  btnBox[i].classList.add("bg-white");
+  btnBox[i].classList.add("text-gray-200");
+
+
+  if (leftValue<1 ) {
+     alert("congrates!!! You have completed all the current task") ;
+    }
   })
+  
 }
 
-
-
-
-
+// history
 document.getElementById("history-delete").addEventListener("click",function(){
-  const delet = document.querySelector("#add-history")
-delet.innerHTML=""
+  const delet = document.querySelector("#add-history");
+delet.innerHTML="";
   
   })
+
+
